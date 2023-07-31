b58863cf86ec321826a6ba4d0d09bb13c48fbba6
nick thomas <nichoth@gmail.com>
2023-07-31 11:46:34 -0700

    fix tapzero dependency

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 // @ts-check
-import { test } from 'tapzero'
+import { test } from '@socketsupply/tapzero'
 
 test('find an element', async t => {
     t.ok(document.querySelector('h1'), 'should find an h1')
